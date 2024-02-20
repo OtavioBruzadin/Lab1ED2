94707d009ffba483737fc5a3b404a82e830eaf89
Otávio Bruzadin <31077442+OtavioBruzadin@users.noreply.github.com>
2024-02-20 16:50:28 -0300

    feat:Created the menu feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,34 @@ package org.example;
 
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(java.lang.String[] args) throws FileNotFoundException {
-        //opt1
-        LinkedList<java.lang.String> movieList = factory.createMovieList("Movies.txt");
-        LinkedList<Movie> objectList = factory.createObjectMovieList(movieList);
-        //opt2
-        for (int i = 0; i < objectList.size(); i++) {
-            System.out.println("#"+i+" "+objectList.pop().toString());
+        LinkedList<Movie> objectList = null;
+        Scanner input = new Scanner(System.in);
+        int opt = 0;
+        while (opt != 3) {
+            System.out.println("digite seu comando:");
+            opt = input.nextInt();
+            if (opt == 1) {
+                try {
+                    LinkedList<java.lang.String> movieList = factory.createMovieList("Movies.txt");
+                    objectList = factory.createObjectMovieList(movieList);
+                    System.out.println("Filmes carregados com sucesso!");
+                }catch (FileNotFoundException e){
+                    System.out.println("Epa! arquivo origem nao encontrado");
+                }
+            }if (opt == 2){
+                try {
+                    for (int i = 0; i < objectList.size(); i++) {
+                        System.out.println("#" + i + " " + objectList.pop().toString());
+                    }
+                }catch (NullPointerException e){
+                    System.out.println("Epa! utilize a opcao 1 primeiro!");
+                }
+            }
         }
-
-
+        System.out.println("Obrigado por utilizar o nosso software UwU");
     }
 }
